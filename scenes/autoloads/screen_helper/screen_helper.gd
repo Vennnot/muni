2a3698c91d26f859c8146c0e5c6425f5a6d7e9c9
dan-linux-laptop <vennnot@gmail.com>
2026-06-03 19:44:59 +0200

    muni screen sizes

--- scenes/autoloads/screen_helper/screen_helper.gd
+++ scenes/autoloads/screen_helper/screen_helper.gd
@@ -8,7 +8,7 @@ var world_size := Vector2(640,360)
 var current_screen: int = DisplayServer.get_primary_screen()
 var screen_size: Vector2i = DisplayServer.screen_get_size(current_screen)
 var screen_position: Vector2i = DisplayServer.screen_get_position(current_screen)
-var world_scale: int = _calc_scaling(screen_size):
+var world_scale: Vector2 = _calc_scaling(screen_size):
 	set(value):
 		if world_scale == value:
 			return
@@ -16,10 +16,10 @@ var world_scale: int = _calc_scaling(screen_size):
 		world_scale_changed.emit()
 
 
-func _calc_scaling(size: Vector2i) -> int:
-	var scale_x: int = size.x / 640
-	var scale_y: int = size.y / 360
-	return mini(scale_x, scale_y)
+func _calc_scaling(size: Vector2) -> Vector2:
+	var scale_x: float = size.x / 640
+	var scale_y: float = size.y / 360
+	return Vector2(scale_x, scale_y)
 
 
 func get_available_screens() -> Array[int]:
@@ -42,3 +42,18 @@ func _update() -> void:
 	screen_position = DisplayServer.screen_get_position(current_screen)
 	world_scale = _calc_scaling(screen_size)
 	screen_changed.emit()
+
+
+func get_integer_scale()->int:
+	return mini(world_scale.x,world_scale.y)
+
+func get_object_offset() -> Vector2i:
+	var integer_scale: int = get_integer_scale()
+	var used := Vector2i(640, 360) * integer_scale
+	return Vector2i((screen_size.x - used.x) / 2, screen_size.y - used.y)
+
+
+#func get_object_offset() -> Vector2i:
+	#var integer_scale: int = get_integer_scale()
+	#var used := Vector2i(640, 360) * integer_scale
+	#return (screen_size - used) / 2
--- scenes/components/windowed_camera/windowed_camera.gd
+++ scenes/components/windowed_camera/windowed_camera.gd
@@ -1,7 +1,7 @@
 class_name WindowedCamera
 extends Camera2D
 
-@export var object_size : Vector2i = Vector2i(16,16)
+@export var object_size : Vector2 = Vector2i(16,16)
 var window : Window : set = _set_window
 
 func _ready() -> void:
@@ -12,18 +12,17 @@ func _ready() -> void:
 func _set_window(w:Window)->void:
 	window = w
 	window.current_screen = ScreenHelper.current_screen
-	window.min_size = object_size * Vector2i(zoom)
+	window.min_size = object_size * Vector2(zoom)
 	window.size = window.min_size
 	set_physics_process(true)
 
 
 func _on_world_scale_changed()->void:
-	zoom = Vector2.ONE * ScreenHelper.world_scale
+	zoom = Vector2.ONE * ScreenHelper.get_integer_scale()
 
 
 func get_window_pos()->Vector2i: 
-	return ScreenHelper.screen_position + (Vector2i(global_position + offset) - object_size / 2) * Vector2i(zoom)
-
+	return ScreenHelper.screen_position + Vector2i(Vector2(global_position + offset - object_size / 2) * ScreenHelper.world_scale)
 
 func _physics_process(delta: float) -> void:
 	if not window:
