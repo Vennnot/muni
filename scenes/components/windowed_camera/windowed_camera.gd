15520f04a779d122495944a33e4daef3e974be78
dan-linux-laptop <vennnot@gmail.com>
2026-06-04 11:53:57 +0200

    Working on framework

--- scenes/components/windowed_camera/windowed_camera.gd
+++ scenes/components/windowed_camera/windowed_camera.gd
@@ -24,18 +24,18 @@ func _on_world_scale_changed()->void:
 
 func get_window_pos() -> Vector2i:
 	var int_scale: int = ScreenHelper.get_integer_scale()
-	var screen := Vector2(ScreenHelper.get_screen_size())
+	var usable := DisplayServer.screen_get_usable_rect(ScreenHelper.current_screen)
+	var screen := Vector2(usable.size)
+	var origin := Vector2i(usable.position)
 	var world := Vector2(640, 360) * int_scale
 	var world_pos := global_position + offset - object_size / 2
 	var ratio := Vector2(world_pos.x / 640.0, world_pos.y / 360.0)
 	var margin := (screen - world)
-	var result := ScreenHelper.screen_position + Vector2i(world_pos * int_scale + margin * ratio)
-
-	var usable := DisplayServer.screen_get_usable_rect(ScreenHelper.current_screen)
+	var result := origin + Vector2i(world_pos * int_scale + margin * ratio)
 	result.y = maxi(result.y, usable.position.y)
-	result.x = maxi(result.x, ScreenHelper.screen_position.x)
+	result.x = maxi(result.x, usable.position.x)
 	result.y = mini(result.y, usable.position.y + usable.size.y - int(window.size.y))
-	result.x = mini(result.x, ScreenHelper.screen_position.x + int(ScreenHelper.get_screen_size().x) - int(window.size.x))
+	result.x = mini(result.x, usable.position.x + usable.size.x - int(window.size.x))
 	return result
 
 func _physics_process(delta: float) -> void:
