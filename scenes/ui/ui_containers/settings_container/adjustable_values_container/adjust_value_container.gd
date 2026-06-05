28813e2675b7e41afe51aa858c391c660aaa2cbc
Daniel Delgado <vennnot@gmail.com>
2026-06-05 11:55:20 +0200

    a

--- scenes/ui/ui_containers/settings_container/adjustable_values_container/adjust_value_container.gd
+++ scenes/ui/ui_containers/settings_container/adjustable_values_container/adjust_value_container.gd
@@ -12,3 +12,13 @@ signal next_pressed
 func _ready() -> void:
 	back_button.pressed.connect(back_pressed.emit)
 	next_button.pressed.connect(next_pressed.emit)
+
+
+func disable()->void:
+	back_button.disabled = true
+	next_button.disabled = true
+
+
+func enable()->void:
+	back_button.disabled = false
+	next_button.disabled = false
--- scenes/ui/ui_containers/settings_container/settings_container.gd
+++ scenes/ui/ui_containers/settings_container/settings_container.gd
@@ -2,18 +2,26 @@ class_name SettingsContainer
 extends VBoxContainer
 
 @onready var adjust_display: AdjustableValueContainer = %AdjustDisplay
+
 @onready var adjust_world_scale: AdjustableValueContainer = %AdjustWorldScale
+@onready var scale_check_box: CheckBox = %ScaleCheckBox
+
 
 var available_screens : int
 
 
 func _ready() -> void:
 	ScreenHelper.screen_changed.connect(_on_screen_changed)
+	available_screens = DisplayServer.get_screen_count()-1
+	
 	adjust_display.back_button.pressed.connect(_on_display_back_pressed)
 	adjust_display.next_button.pressed.connect(_on_display_next_pressed)
+	
 	adjust_world_scale.back_button.pressed.connect(_on_world_back_pressed)
 	adjust_world_scale.next_button.pressed.connect(_on_world_next_pressed)
-	available_screens = DisplayServer.get_screen_count()-1
+	scale_check_box.pressed.connect(_on_scale_checkbox_pressed)
+	adjust_world_scale.disable()
+	
 	_on_screen_changed()
 
 func _on_display_back_pressed()->void:
@@ -29,12 +37,23 @@ func _on_display_next_pressed()->void:
 	ScreenHelper.set_screen(current_screen+1)
 
 
+
+func _on_screen_changed()->void:
+	adjust_display.value_label.text = str(ScreenHelper.current_screen)
+
+
+#region world scale
+func _on_scale_checkbox_pressed()->void:
+	if scale_check_box.pressed:
+		adjust_world_scale.enable()
+	else:
+		adjust_world_scale.disable()
+
+
 func _on_world_back_pressed()->void:
 	pass
 
 
 func _on_world_next_pressed()->void:
 	pass
-
-func _on_screen_changed()->void:
-	adjust_display.value_label.text = str(ScreenHelper.current_screen)
+#endregion
